4ac600b1a56e01a62884d3132791e11d0fa90cbe
wfsun <wfsun@iaspec.com>
2022-01-14 17:15:32 +0800

    test branch_1 edit

--- src/com/wfsun/test/Test.java
+++ src/com/wfsun/test/Test.java
@@ -2,6 +2,6 @@ package com.wfsun.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world - branch_1");
 	}
 }
